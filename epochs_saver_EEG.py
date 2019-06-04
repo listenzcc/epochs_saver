d9d8a2e65aee705e9ce02d68cb205fc8ce06ee0c
zcc <listenzcc@mail.bnu.edu.cn>
2019-06-04 10:58:33 +0800

    add epochs_saver_EEG and _MEG

---
+++ epochs_saver_EEG.py
@@ -0,0 +1,67 @@
+# coding: utf-8
+'''
+This script is to load EEG RSVP dataset,
+and save epochs as fif file.
+'''
+
+import mne
+import os
+import time
+
+'''
+# Function: Setting evrionment for the script.
+# Output: root_path, directory of project.
+# Output: time_stamp, string of beginning time of the script.
+# Output: id_string, customer identifier string.
+# Output: results_dir, directory for storing results.
+'''
+root_dir = os.path.join('D:\\', 'RSVP_MEG_experiment')
+time_stamp = time.strftime('%Y-%m-%d-%H-%M-%S')
+results_dir = os.path.join(root_dir, 'epochs_saver', 'epochs')
+if not os.path.exists(results_dir):
+    os.mkdir(results_dir)
+
+'''
+# Function: Reading raw_files.
+'''
+file_dir = os.path.join(root_dir, 'rawdata', '20190402_RSVP_EEG_maxuelin',
+                        'mxl%d.cnt')
+freq_l, freq_h = 0.1, 7  # Hz
+fir_design = 'firwin'
+
+'''
+# Function: Preparing motage for EEG dataset
+# Output: motage, motage of standard_1020
+'''
+motage = mne.channels.read_montage('standard_1020')
+
+for run_idx in range(1, 11):
+    print(run_idx)
+
+    raw = mne.io.read_raw_cnt(file_dir % run_idx,
+                              motage, preload=True)
+    raw = raw.filter(freq_l, freq_h, fir_design=fir_design)
+
+    '''
+    # Function: Get epochs.
+    # Output: epochs, resampled epochs of norm and odd stimuli.
+    '''
+    picks = mne.pick_types(raw.info, eeg=True, exclude='bads')
+    stim_channel = 'STI 014'
+    events = mne.find_events(raw, stim_channel=stim_channel)
+    event_id = dict(odd=1, norm=2)
+    tmin, t0, tmax = -0.2, 0, 1  # Seconds
+    freq_resample = 200  # Hz
+    reject = dict()
+
+    epochs = mne.Epochs(raw, picks=picks, events=events, event_id=event_id,
+                        tmin=tmin, tmax=tmax, baseline=None, detrend=1,
+                        reject=reject, preload=True)
+    epochs.resample(freq_resample, npad='auto')
+
+    '''
+    # Function: Save epochs.
+    # Output: None.
+    '''
+    epochs.save(os.path.join(results_dir, 'eeg_mxl_epochs_%d-epo.fif' %
+                             run_idx), verbose=True)
